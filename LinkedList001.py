8f7dbe37db85114c510579bf5f99256c7d02ad71
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-09 23:48:58 -0300

    Added pop_first function

--- LinkedList001.py
+++ LinkedList001.py
@@ -54,7 +54,18 @@ class LinkedList:
             self.head = new_node
         self.length += 1
         return True
-
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp.value
+    
 
 
 
