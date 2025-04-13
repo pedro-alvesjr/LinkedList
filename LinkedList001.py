af7f2d1d440addabb3e9798b37722738bf1cb789
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-12 22:29:46 -0300

    Complete LinkedList
    All of the methods were added. Completely functional.

--- LinkedList001.py
+++ LinkedList001.py
@@ -27,17 +27,18 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+        return True
 
     def pop(self):
         if self.length == 0:
             return None
+        temp = self.head
         pre = self.head
-        temp = self.head.next
-        while temp != self.tail:
+        while temp.next is not None:
+            pre = temp
             temp = temp.next
-            pre = pre.next
-        pre.next = None
         self.tail = pre
+        self.tail.next = None
         self.length -= 1
         if self.length == 0:
             self.head = None
@@ -64,15 +65,64 @@ class LinkedList:
         self.length -= 1
         if self.length == 0:
             self.tail = None
-        return temp.value
+        return temp
     
     def get(self, index):
-        if index < 0 or index > self.length:
+        if index < 0 or index >= self.length:
             return None
         temp = self.head
-        for i in range(index):
+        for _ in range(index):
             temp = temp.next
         return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value) 
+        new_node = Node(value)
+        prev = self.get(index - 1)
+        next = prev.next
+        prev.next = new_node
+        new_node.next = next
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        next = temp.next
+        prev.next = next
+        temp.next = None
+        self.length -= 1
+        return temp
+        
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
 
 
@@ -84,10 +134,15 @@ my_linked_list.append(5)
 my_linked_list.pop()
 my_linked_list.prepend(0)
 
-print('Get:', my_linked_list.get(2))
+print('Get index:', my_linked_list.get(2))
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length, '\n')
 
 print('Linked List:')
+my_linked_list.print_list()
+
+my_linked_list.reverse()
+
+print('\nLL after reverse():')
 my_linked_list.print_list()
